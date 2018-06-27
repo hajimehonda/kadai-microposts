aa5d1f3fa65f7fd67934db9139505af49a5254a1
hajime honda <hajime.honda@rakuten.com>
2018-06-27 04:31:55 +0000

    kkkkkkkkkkkkkkkk

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
     <div class="row">
+         <aside class="col-xs-4">
             <div class="panel panel-default">
                 <div class="panel-heading">
                     <h3 class="panel-title">{{ $user->name }}</h3>
