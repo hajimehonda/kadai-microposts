42ddde619c495d824f7b72552f924ed15fbb378e
hajime honda <hajime.honda@rakuten.com>
2018-06-27 04:16:48 +0000

    llll

--- resources/views/microposts/microposts.blade.php
+++ resources/views/microposts/microposts.blade.php
@@ -27,6 +27,17 @@
                 {!! Form::open(['route' => ['user.favorite', $micropost->id], 'method' => 'store']) !!}
                         {!! Form::submit('Favorite', ['class' => 'btn btn-primary btn-xs']) !!}
                         {!! Form::close() !!}
+                @endif
+               
+               @if(Auth::user()->is_favoriting($micropost->id))
+                     {!! Form::open(['route' => ['user.unfavorite', $micropost->id], 'method' => 'delete']) !!}
+                     {!! Form::submit('Unfavorite', ['class' => "btn btn-danger btn-xs"]) !!}
+                     {!! Form::close() !!}
+                
+                @else
+                {!! Form::open(['route' => ['user.favorite', $micropost->id], 'method' => 'store']) !!}
+                        {!! Form::submit('Favorite', ['class' => 'btn btn-primary btn-xs']) !!}
+                        {!! Form::close() !!}
                 
                 
                 
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -2,7 +2,6 @@
 
 @section('content')
     <div class="row">
-        <aside class="col-xs-4">
             <div class="panel panel-default">
                 <div class="panel-heading">
                     <h3 class="panel-title">{{ $user->name }}</h3>
